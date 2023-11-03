8c0d73dfb1f071d93af8b35193c5cdf2216699bd
migue <miguel.lau@etecsa.cu>
2023-11-03 16:39:21 -0400

    arreglo

--- src/java/sys/Register.java
+++ src/java/sys/Register.java
@@ -10,52 +10,32 @@ public class Register {
 
     public Register() { }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public String getPhone() {
-        return phone;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getRole() {
-        return role;
     }
 
     public void setRole(String role) {
         this.role = role;
     }
-
+    
+    public String valor() {
+        return this.firstName + "-" + this.lastName;
+    }
+    
 }
