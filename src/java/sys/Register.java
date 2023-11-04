f0b2fbd100d08860682253ee709944452e12ffd8
cyro99 <149835528+cyro99@users.noreply.github.com>
2023-11-03 20:22:16 -0400

    Modificando clase Register

--- src/java/sys/Register.java
+++ src/java/sys/Register.java
@@ -1,29 +1,14 @@
 package sys;
 
 public class Register {
-    private String firstName = "";
-    private String lastName = "";
-    private String phone = "";
-    private String email = "";
+    private String username = "";
     private String password = "";
     private String role = "";
 
     public Register() { }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setPassword(String password) {
@@ -35,7 +20,7 @@ public class Register {
     }
     
     public String valor() {
-        return this.firstName + "-" + this.lastName;
+        return this.username + " -> " + this.role;
     }
 
     
